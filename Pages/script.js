b05023526979211bb9a321338a8812916baf6ccb
antonioxmrm <antoniomacedo210504@gmail.com>
2024-11-05 15:09:06 -0300

    atualizado_05/11_15:09

--- Pages/script.js
+++ Pages/script.js
@@ -1,3 +1,19 @@
 function toggleSidebar() {
   document.querySelector('.sidebar').classList.toggle('collapsed');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".toggle-button");
+  const itemList = document.querySelector(".item-list");
+
+  toggleButton.addEventListener("click", function () {
+      itemList.classList.toggle("hidden");
+      
+      // Atualiza o texto do botão com base no estado da lista
+      if (itemList.classList.contains("hidden")) {
+          toggleButton.textContent = "Mostrar Itens";
+      } else {
+          toggleButton.textContent = "Ocultar Itens";
+      }
+  });
+});
