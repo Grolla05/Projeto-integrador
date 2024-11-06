2a5a28493122fa6c347869f5c68eedf916d90534
Felipe Grolla Freitas <120500170+Grolla05@users.noreply.github.com>
2024-11-06 00:27:12 -0300

    Update home e 404
    Atualização da tela de home e 404 de maneira que fique mais organizado e bonito, além da atualização do script de home

--- Pages/script.js
+++ Pages/script.js
@@ -17,3 +17,65 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   });
 });
+
+function toggleGamelist() {
+    const gameList = document.querySelector('.game-list');
+    const toggleIcon = document.querySelector('.PUCBET-icon');
+
+    if (gameList.classList.contains('expanded')) {
+        // Colapsa a lista
+        gameList.style.maxHeight = '0';
+        gameList.classList.remove('expanded');
+        toggleIcon.classList.remove('expanded');
+        toggleIcon.textContent = '▼';
+    } else {
+        // Expande a lista: calcula a altura real do conteúdo
+        gameList.classList.add('expanded');
+        toggleIcon.classList.add('expanded');
+        toggleIcon.textContent = '▲';
+
+        // Define o max-height para a altura total dos itens
+        const fullHeight = gameList.scrollHeight + "px";
+        gameList.style.maxHeight = fullHeight;
+    }
+}
+
+function toggleCassinolist() {
+  const CassinoList = document.querySelector('.Cassino-list');
+  const toggleIcon = document.querySelector('.Cassino-icon');
+
+  if (CassinoList.classList.contains('expanded')) {
+      // Colapsa a lista
+      CassinoList.style.maxHeight = '0';
+      CassinoList.classList.remove('expanded');
+      toggleIcon.classList.remove('expanded');
+      toggleIcon.textContent = '▼';
+  } else {
+      // Expande a lista: calcula a altura real do conteúdo
+      CassinoList.classList.add('expanded');
+      toggleIcon.classList.add('expanded');
+      toggleIcon.textContent = '▲';
+
+      // Define o max-height para a altura total dos itens
+      const fullHeight = CassinoList.scrollHeight + "px";
+      CassinoList.style.maxHeight = fullHeight;
+  }
+}
+
+ // Função para abrir o pop-up
+ function openPopup() {
+    document.getElementById("modalBg").style.display = "flex";
+}
+
+// Função para fechar o pop-up
+function closePopup() {
+    document.getElementById("modalBg").style.display = "none";
+}
+
+// Fechar o pop-up ao clicar fora da área do modal
+window.onclick = function(event) {
+    const modalBg = document.getElementById("modalBg");
+    if (event.target === modalBg) {
+        closePopup();
+    }
+};
