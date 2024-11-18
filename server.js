f64d8195284f09fed86f9648637943c11177ce2e
GabrielPozeti <161641369+GabrielPozeti@users.noreply.github.com>
2024-11-18 13:27:40 -0300

    Add

--- server.js
+++ server.js
@@ -10,6 +10,34 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.get('/CassinoHUB', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'CassinoHUB.html'));
+});
+
+app.get('/Contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Contact.html'));
+});
+
+app.get('/About', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'About.html'));
+});
+
+app.get('/Services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Services.html'));
+});
+
+app.get('/Portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Portfolio.html'));
+});
+
+app.get('/Blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Blog.html'));
+});
+
+app.get('/FAQ', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'FAQ.html'));
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
