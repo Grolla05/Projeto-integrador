39d151804f54276c058f286f9f846a1611f8575f
GabrielPozeti <161641369+GabrielPozeti@users.noreply.github.com>
2024-11-24 18:44:23 -0300

    .

--- server.js
+++ server.js
@@ -75,8 +75,3 @@ server.get('/roulette', (req, res) => {
 server.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
-
-// Iniciar o servidor
-server.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
