c6501620ff51a967693ed3c08823f84d2dddef17
GabrielPozeti <161641369+GabrielPozeti@users.noreply.github.com>
2024-11-24 19:05:29 -0300

    update responsividade pagina 404

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDacw_vn8GMxF-J42Hr1n1JuYTZow3kSeM",
-    authDomain: "projeto-integrador-pucbet.firebaseapp.com",
-    projectId: "projeto-integrador-pucbet",
-    storageBucket: "projeto-integrador-pucbet.firebasestorage.app",
-    messagingSenderId: "898894824955",
-    appId: "1:898894824955:web:a1d85eb33ebcfbdd86375e",
-    measurementId: "G-JW2HFW9GVF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-=======
->>>>>>> parent of d13ed83 (.)
 const express = require('express');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
@@ -28,37 +6,6 @@ const session = require('express-session');
 const app = express();
 const port = 3000;
 
-<<<<<<< HEAD
-// Middleware para analisar o corpo das requisições
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-
-// Rotas de autenticação
-server.post('/register', (req, res) => {
-    const { email, password } = req.body;
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            res.status(201).send('Usuário registrado com sucesso');
-        })
-        .catch((error) => {
-            res.status(400).send(error.message);
-        });
-});
-
-server.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            res.status(200).send('Usuário logado com sucesso');
-        })
-        .catch((error) => {
-            res.status(400).send(error.message);
-        });
-});
-
-// Outras rotas
-server.get('/', (req, res) => {
-=======
 // Configurar o body-parser para lidar com dados de formulários
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Adicionado para lidar com JSON
@@ -99,7 +46,6 @@ const db = new sqlite3.Database('./projeto_integrador.db', (err) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
->>>>>>> parent of d13ed83 (.)
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
@@ -111,24 +57,6 @@ server.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about_us.html'));
 });
 
-<<<<<<< HEAD
-server.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Contact.html'));
-});
-
-server.get('/roulette', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Roulette.html'));
-});
-
-// Iniciar o servidor
-server.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
-
-// Iniciar o servidor
-server.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-=======
 app.get('/404', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
@@ -258,5 +186,4 @@ app.post('/logout', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
->>>>>>> parent of d13ed83 (.)
 });
